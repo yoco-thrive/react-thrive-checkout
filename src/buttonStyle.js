76ef06135846ba0fd2ede52ff964c2c20cb2d9d9
Gert <gert@yoco.com>
2019-05-10 14:48:20 +0200

    Add initial styles for the button

--- src/buttonStyle.js
+++ src/buttonStyle.js
@@ -1,2 +1,13 @@
 export default {
+  alignItems: 'flex-start',
+  backgroundColor: 'hsl(199, 91%, 64%)',
+  borderRadius: '3px',
+  borderWidth: '2px',
+  color: 'hsl(0, 0%, 100%)',
+  display: 'inline-block',
+  fontFamily: 'system-ui',
+  fontWeight: 500,
+  lineHeight: '16px',
+  padding: '14px 30px',
+  textAlign: 'center',
 }
